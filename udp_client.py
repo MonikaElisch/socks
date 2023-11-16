88ef81738ea8df0c4f6f2e1f18a43f54247b6a88
MonikaElisch <125794798+MonikaElisch@users.noreply.github.com>
2023-11-16 16:33:47 +0700

    Update udp_client.py

--- udp_client.py
+++ udp_client.py
@@ -1,1 +1,10 @@
-
+import time
+import socket
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+port =25661
+x  =input("Enter two numbers: ")
+client.sendto(x.encode('ascii'), (socket.gethostname(),port))
+data = client.recvfrom(1024).decode('ascii')
+print ("6488185: ", time.asctime()," ",data.decode('ascii'))
+client.close()
